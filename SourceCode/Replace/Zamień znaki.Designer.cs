bff556526ca31a96558c5333710f2927551fd629
Piotr <piotr_dudzinski@outlook.com>
2018-11-12 10:18:20 +0100

    Create Sierotki Word AddIn

---
+++ SourceCode/Replace/Zamień znaki.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Replace
+{
+    partial class Replace : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Replace()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Replace));
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.groupBox = this.Factory.CreateRibbonGroup();
+            this.buttonRun = this.Factory.CreateRibbonButton();
+            this.button1 = this.Factory.CreateRibbonButton();
+            this.tab1.SuspendLayout();
+            this.groupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.Groups.Add(this.groupBox);
+            this.tab1.Label = "TabAddIns";
+            this.tab1.Name = "tab1";
+            // 
+            // groupBox
+            // 
+            this.groupBox.Items.Add(this.buttonRun);
+            this.groupBox.Items.Add(this.button1);
+            this.groupBox.Label = "Sierotki w tekście";
+            this.groupBox.Name = "groupBox";
+            // 
+            // buttonRun
+            // 
+            this.buttonRun.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.buttonRun.Image = ((System.Drawing.Image)(resources.GetObject("buttonRun.Image")));
+            this.buttonRun.Label = "Wykonaj";
+            this.buttonRun.Name = "buttonRun";
+            this.buttonRun.ShowImage = true;
+            this.buttonRun.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.buttonRun_Click);
+            // 
+            // button1
+            // 
+            this.button1.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Label = "Cofnij";
+            this.button1.Name = "button1";
+            this.button1.ShowImage = true;
+            this.button1.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button1_Click);
+            // 
+            // Replace
+            // 
+            this.Name = "Replace";
+            this.RibbonType = "Microsoft.Word.Document";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Zamień_znaki_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.groupBox.ResumeLayout(false);
+            this.groupBox.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup groupBox;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton buttonRun;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button1;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal Replace Zamień_znaki
+        {
+            get { return this.GetRibbon<Replace>(); }
+        }
+    }
+}
---
+++ SourceCode/Replace/Zamień znaki.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+using Microsoft.Office.Interop.Word;
+
+namespace Replace
+{
+    public partial class Replace
+    {
+        private void Zamień_znaki_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void buttonRun_Click(object sender, RibbonControlEventArgs e)
+        {
+            string[] ArrayOfCharacters = new string[6];
+            ArrayOfCharacters[0] = "a";
+            ArrayOfCharacters[1] = "o";
+            ArrayOfCharacters[2] = "u";
+            ArrayOfCharacters[3] = "w";
+            ArrayOfCharacters[4] = "i";
+            ArrayOfCharacters[5] = "z";
+
+            Microsoft.Office.Interop.Word.Document doc = Globals.ThisAddIn.Application.ActiveDocument;
+
+            foreach(string character in ArrayOfCharacters)
+            {
+                doc.Content.Find.Execute(character + " ", false, true, false, false, false, true, 1, false, character + "\u00A0", 2, false, false, false, false);
+            }
+        }
+
+        private void button1_Click(object sender, RibbonControlEventArgs e)
+        {
+            string[] ArrayOfCharacters = new string[6];
+            ArrayOfCharacters[0] = "a";
+            ArrayOfCharacters[1] = "o";
+            ArrayOfCharacters[2] = "u";
+            ArrayOfCharacters[3] = "w";
+            ArrayOfCharacters[4] = "i";
+            ArrayOfCharacters[5] = "z";
+
+            Microsoft.Office.Interop.Word.Document doc = Globals.ThisAddIn.Application.ActiveDocument;
+
+            foreach (string character in ArrayOfCharacters)
+            {
+                doc.Content.Find.Execute(character + "\u00A0", false, true, false, false, false, true, 1, false, character + " ", 2, false, false, false, false);
+            }
+        }
+    }
+}
